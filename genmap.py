5cfa274672c2d64a499c2ea575b3f6b85203d295
Chad Whitacre <chad@zetaweb.com>
2015-06-26 18:41:40 -0400

    Generate candidate shapes
    Next is to start placing them on the map.

--- genmap.py
+++ genmap.py
@@ -1,32 +1,74 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import math
 import random
 
 
-map_ = [['a'] + ['-' for i in range(512)] + ['a'] for j in range(512)]
-map_.insert(0, ['a' for i in range(514)])
-map_.append(['a' for i in range(514)])
+class ResourceMap(list):
+
+    def __init__(self, canvas_size, sum_of_durations):
+        self.H = self.W = canvas_size
+        self.area = self.H * self.W
+        self.sum_of_durations = sum_of_durations
+
+        # Build the base map. It's surrounded by alleys, so it's effectively
+        # 1-indexed.
+        padded = lambda row: ['a'] + row + ['a']
+        top_and_bottom = lambda: padded(['a' for i in range(self.W)])
+        self.extend([padded(['-' for i in range(self.W)]) for j in range(self.H)])
+        self.insert(0, top_and_bottom())
+        self.append(top_and_bottom())
+
+    def __str__(self):
+        return '\n'.join([''.join([c for c in row]) for row in self])
+
+    def add_shape(self, duration):
+
+        # Find first empty cell.
+        x = y = 0
+        while 1:
+            if self[x][y] == '-':
+                break
+            x += 1
+            y += 1
+
+        target_area = int(self.area * (duration / self.sum_of_durations))
+        assert target_area >= 16
+
+        candidates = self._get_candidate_shapes(target_area)
+        shape = random.choice(candidates)
+        print(shape)
+
+    def _get_candidate_shapes(self, target_area):
+        lo = 4
+        hi = None
+
+        while 1:
+            hi = int(math.floor(target_area / lo))
+            if lo / hi >= 0.2:
+                break
+            else:
+                lo += 1
+
+        candidates = []
+        for i in range(lo, hi+1):
+            for j in range(lo, hi+1):
+                candidates.append((i,j))
+
+        return candidates
 
 
 def fake_data():
     for i in range(50):
-        yield random.randint(1, 10)
+        yield 32 * random.randint(1, 10)
 
 
-def print_map(map_):
-    for row in map_:
-        for c in row:
-            print(c, end='')
-        print()
+if __name__ == '__main__':
+    durations = list(fake_data())
+    resource_map = ResourceMap(canvas_size=512, sum_of_durations=sum(durations))
+    for duration in durations:
+        resource_map.add_shape(duration)
+    import pdb; pdb.set_trace()
 
-
-def add_to_map(duration):
-    pass
-
-
-for duration in fake_data():
-    add_to_map(duration)
-
-
-print_map(map_)
+    print(resource_map)
