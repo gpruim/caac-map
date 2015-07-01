4d13aeb9e27467a61f427ce848e95bcfb759de6c
Chad Whitacre <chad@zetaweb.com>
2015-07-01 11:17:39 -0400

    Draw alleys around shapes

--- genmap.py
+++ genmap.py
@@ -5,6 +5,11 @@ import math
 import random
 
 
+A = '\u2591'    # Alley
+B = '\u2593'    # Block/Building
+C = ' '         # Canvas
+
+
 class MagnitudeMap(list):
 
     def __init__(self, canvas_size, sum_of_magnitudes):
@@ -12,23 +17,26 @@ class MagnitudeMap(list):
         self.area = self.H * self.W
         self.sum_of_magnitudes = sum_of_magnitudes
 
-        # Build the base map. It's surrounded by alleys, so it's effectively
-        # 1-indexed.
-        padded = lambda row: ['a'] + row + ['a']
-        top_and_bottom = lambda: padded(['a' for i in range(self.W)])
-        self.extend([padded(['-' for i in range(self.W)]) for j in range(self.H)])
-        self.insert(0, top_and_bottom())
-        self.append(top_and_bottom())
+        # Build the base map. It's surrounded by alleys, which are four units wide.
+        padded = lambda row: [A,A,A,A] + row + [A,A,A,A]
+        top_or_bottom = lambda: padded([A for i in range(self.W)])
+        self.extend([padded([C for i in range(self.W)]) for j in range(self.H)])
+        for i in range(4):
+            self.insert(0, top_or_bottom())
+            self.append(top_or_bottom())
 
     def __str__(self):
-        return '\n'.join([''.join([c for c in row]) for row in self])
+        return '\n'.join([''.join([c for c in row]) for row in self]).encode('UTF-8')
+
+    def __unicode__(self):
+        return str(self).decode('UTF-8')
 
     def add(self, magnitude):
 
         # Find first empty cell.
         x = y = 0
         while 1:
-            if self[x][y] == '-':
+            if self[x][y] == C:
                 break
             x += 1
             if x > self.W:
@@ -54,7 +62,34 @@ class MagnitudeMap(list):
         w, h = shape
         for i in range(x, x+w+1):
             for j in range(y, y+h+1):
-                self[i][j] = '#'
+                self[i][j] = B
+
+        self._draw_alleys_around_shape(w, h, x, y)
+
+
+    def _draw_alleys_around_shape(self, w, h, x, y):
+        left, right = x, x+w+1
+        top, bottom = y, y+h+1
+
+        def draw_alley(i,j):
+            assert self[i][j] in (C, A)
+            self[i][j] = A
+
+        for i in range(right, right+4):
+            for j in range(top-4, bottom+4):
+                draw_alley(i,j)
+
+        for i in range(left-4, left):
+            for j in range(top-4, bottom+4):
+                draw_alley(i,j)
+
+        for i in range(left, right):
+            for j in range(top-4, top):
+                draw_alley(i,j)
+
+        for i in range(left, right):
+            for j in range(bottom, bottom+4):
+                draw_alley(i,j)
 
 
     def _get_candidate_shapes(self, target_area):
@@ -64,7 +99,7 @@ class MagnitudeMap(list):
         while 1:
             hi = int(math.floor(target_area / lo))
             if lo / hi >= 0.2:
-                break
+                break  # maintain a certain minimum aspect ratio
             else:
                 lo += 1
 
