a3c00312aa7fdd93c8b71a0ddb1a737d45dee35e
Chad Whitacre <chad@zetaweb.com>
2015-07-10 13:07:32 -0400

    Explore access to SVG from JavaScript/jQuery

--- www/map.js
+++ www/map.js
@@ -4,5 +4,10 @@ Map.init = function() {
     $('body').load('map.svg', function() {
         $('svg').svg();
         var svg = $('svg').svg('get');
+
+        $('rect').attr('fill', '#0099FF');
+
+        $('#456').attr('fill', '#025127');
+        $('#567').attr('class', 'selected'); // .addClass doesn't work for whatever reason
     });
 };
