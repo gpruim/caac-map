c56176e99f07928db537fb2b671b7522df3cf6cf
Chad Whitacre <chad@zetaweb.com>
2015-07-10 13:52:29 -0400

    Actually, let's use 'magnitude' as the key instead

--- csv2json.py
+++ csv2json.py
@@ -6,5 +6,5 @@ import csv, json, random, sys
 incoming = csv.reader(sys.stdin)
 headers = incoming.next()
 duration = lambda: random.randint(1, 20)
-outgoing = [dict(zip(headers, row), id=i, duration=duration()) for i, row in enumerate(incoming)]
+outgoing = [dict(zip(headers, row), id=i, magnitude=duration()) for i, row in enumerate(incoming)]
 json.dump(outgoing, sys.stdout, sort_keys=True, indent=4, separators=(',', ': '))
