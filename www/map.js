aa0d9e331d99f4b120ef22d88bb1d9c9e796f0e3
Chad Whitacre <chad@zetaweb.com>
2015-07-10 16:58:56 -0400

    Implement some popups

--- www/map.js
+++ www/map.js
@@ -1,15 +1,41 @@
 Map = {};
 
+Map.popup = function(e) {
+    var rect = $(this);
+    var resource = Map.resources[rect.attr('id')];
+
+    $('.selected').attr('class', '');
+    rect.attr('class', 'selected');
+
+    $('.resourceName').html(resource.resourceName).attr('href', resource.resourceUrl);
+    $('.popup h1').css('background-image', 'url("'+resource.imageUrl+'")');
+    $('.resourceDescription').html(resource.resourceDescription);
+
+    $('.orgName a').attr('href', resource.orgUrl);
+    $('.orgName a').html(resource.orgName);
+    $('.orgDescription').html(resource.orgDescription);
+
+    $('.cta a').attr('href', resource.resourceUrl);
+
+    $('.popup').show();
+    e.stopPropagation();
+};
+
+Map.popdown = function() {
+    $('.popup').hide();
+    $('.selected').attr('class', '');
+};
+
 Map.init = function() {
-    $('body').load('map.svg', function() {
-        $('svg').svg();
-        var svg = $('svg').svg('get');
 
-        $('rect').attr('fill', '#0099FF');
+    jQuery.get('resources.json', function(resources) {
+        Map.resources = resources;
+        $('#map').load('map.svg', function() {
+            $('svg').svg();
+            var svg = $('svg').svg('get');
 
-        $('#10').attr('fill', '#025127');
-        $('#20').attr('class', 'selected'); // .addClass doesn't work for whatever reason
-
-        $('rect').click(function() { alert('Yo!') });
+            $('rect').click(Map.popup);
+            $('#map').click(Map.popdown);
+        });
     });
 };
