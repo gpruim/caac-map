87d679d84bcba7a947bea23c1ab45a6dfd36c137
Chad Whitacre <chad@zetaweb.com>
2015-08-28 12:23:06 -0400

    Update fetch.py to work with new Google Sheet

--- fetch.py
+++ fetch.py
@@ -20,28 +20,33 @@ import requests
 
 
 def _get(url):
+    print("Getting {} ...".format(url))
     response = requests.get(url)
     if not response.status_code == 200:
+        print("Problem downloading {} ...".format(url))
         print(response.status_code)
         print(response.text)
         raise SystemExit
     if 'Sign in to continue to Sheets' in response.text:
-        #open('bad.html', 'w+').write(response.content)
+        print("Problem downloading {} ...".format(url))
         print("They're asking us to sign in. Try 'File > Publish to the web ...'.")
         raise SystemExit
     return response.text
 
 
 def fetch_worksheets(sheet_key):
-    url = "https://spreadsheets.google.com/feeds/worksheets/{}/public/basic"
-    url = url.format(sheet_key)
-    raw = _get(url)
+    csv_url = "https://docs.google.com/spreadsheets/d/{}/pub?gid={}&single=true&output=csv"
+    listing_url = "https://spreadsheets.google.com/feeds/worksheets/{}/public/basic"
+    listing_url = listing_url.format(sheet_key)
+    raw = _get(listing_url)
     tree = ET.fromstring(raw)
     ns = {'atom': 'http://www.w3.org/2005/Atom'}
     entries = tree.findall('./atom:entry', ns)
     xtitle = lambda entry: entry.find('./atom:title', ns).text
     xcsvurl = lambda entry: entry.find("./atom:link[@type='text/csv']", ns).attrib['href']
-    return [(xtitle(e), xcsvurl(e)) for e in entries]
+    xgid = lambda csvurl: re.findall('gid=(\d+)', csvurl)[0]
+    puburl = lambda gid: csv_url.format(sheet_key, gid)
+    return [(xtitle(e), puburl(xgid(xcsvurl(e)))) for e in entries]
 
 
 def fetch_resources_by_topic(worksheets):
@@ -51,7 +56,7 @@ def fetch_resources_by_topic(worksheets):
         raw = raw.encode('utf8')  # csv can only use str
         reader = csv.reader(StringIO(raw))
         headers = reader.next()
-        UID = 1
+        UID = 0
         resources = {row[UID]: dict(zip(headers, row)) for row in reader}
         topics[topic] = resources
     return topics
@@ -62,12 +67,14 @@ def validate_uids(topics):
     for name, topic in topics.items():
         for resource in topic.values():
             for field in ('uid', 'comes_before', 'comes_after'):
-                if not re.match(r'^[a-z-]+$', resource[field]):
+                if not re.match(r'^[a-z-]*$', resource[field]):
                     bad.add((name, field, resource[field].encode('ascii', errors='replace')))
     if bad:
         print("{} bad uid(s)!".format(len(bad)))
+        print("{:24} {:24} {:24}".format("sheet", "field", "value"))
+        print("{:-<24} {:-<24} {:-<24}".format("", "", ""))
         for sheet, field, value in sorted(bad):
-            print(sheet, field, value)
+            print("{:24} {:24} {:24}".format(sheet, field, value))
         raise SystemExit
 
 
