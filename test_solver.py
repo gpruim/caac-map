b263b2cdf39124246f70452d95d6fe00810cf5b5
Chad Whitacre <chad@zetaweb.com>
2015-10-16 10:57:13 -0400

    Failing test for observed bug in solver.next

--- test_solver.py
+++ test_solver.py
@@ -1,5 +1,6 @@
 import pathways_solver as ps
 
+
 # Problem
 
 def test_we_can_express_an_empty_problem():
@@ -10,3 +11,12 @@ def test_we_can_express_an_empty_problem():
 
 def test_solve_solves_pathways():
     assert ps.solve({}, {}) == [{}]
+
+
+# next
+
+def test_next_sibling_properly_identifies_base_case():
+    P = ps.Problem({'a': None, 'b': None}, {'foo': ['x', 'y']})
+    P.indices = [(0,0)]
+    s = {'foo': [('a', 'x'), ('b', 'y')]}
+    assert ps.next(P, s) == None
